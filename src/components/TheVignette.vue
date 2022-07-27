5b0a443eb68976469c620b95d74050496180157e
the-fukui <account@the-fukui.com>
2022-07-27 19:17:02 +0900

    add vue support

---
+++ src/components/TheVignette.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="vignette" />
+</template>
+
+<style lang="scss" scoped>
+.vignette {
+  box-shadow: inset 0 0 200px black;
+  opacity: 0.7;
+
+  @include mq-down(md) {
+    box-shadow: inset 0 0 100px black;
+  }
+}
+</style>

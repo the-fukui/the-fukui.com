bee35e3b075c4d771c3437703871f4f6ed34ec40
the-fukui <account@the-fukui.com>
2022-08-08 22:19:25 +0900

    [wip] vite

--- src/libs/microcms.ts
+++ src/libs/microcms.ts
@@ -1,5 +1,7 @@
 import { createClient } from 'microcms-js-sdk'
 
+console.log(import.meta.env.MICROCMS_SERVICE_DOMAIN, 'aaaa')
+
 const _client = createClient({
   serviceDomain: import.meta.env.MICROCMS_SERVICE_DOMAIN,
   apiKey: import.meta.env.MICROCMS_API_KEY,

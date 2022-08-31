6bf0e507bd520d318cd23b098bb3afdf5b8f934b
the-fukui <account@the-fukui.com>
2022-08-31 22:32:26 +0900

    set custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://the-fukui.github.io",
+  site: "https://renew.the-fukui.com",
   integrations: [vue(), image()],
   vite: {
     plugins: [tsconfigPaths()],

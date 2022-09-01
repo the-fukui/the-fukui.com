01f2b0c465dcb8d683e120bdfbec1f12f32c476f
the-fukui <account@the-fukui.com>
2022-09-01 18:43:13 +0900

    add prefetch integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,18 @@ import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import image from "@astrojs/image";
-
 import compress from "astro-compress";
+
+import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://renew.the-fukui.com",
-  integrations: [vue(), image(), compress({ img: false })],
+  integrations: [vue(), image(), compress({
+    img: false
+  }), prefetch({
+    selector: "a[href^='/']"
+  })],
   vite: {
     plugins: [tsconfigPaths()],
     ssr: {
